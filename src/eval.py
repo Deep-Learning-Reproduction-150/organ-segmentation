8c8f1476c6d43f3ce61ef2d92f7d750141f7c456
Tom Oud <tomtomoud@hotmail.com>
2022-04-07 13:58:52 +0200

    progress on evaluator

--- src/eval.py
+++ src/eval.py
@@ -8,7 +8,7 @@ Group: 150
 import torch
 
 from src.utils import Logger
-from src.losses import CombinedLoss
+from src.losses import CombinedLoss, DiceCoefficient
 from sklearn.model_selection import KFold
 
 
@@ -37,16 +37,17 @@ class ChenEvaluator(BaseEvaluator):
     # Configuration
     k_folds = 5
     n_epochs = 2
-    loss_function = CombinedLoss()
     results = {}
 
-    def evaluate(self, trained_model, evaluation_set, job_path):
+    def evaluate(self, trained_model, evaluation_set, job):
         """
         Evaluate the model based on the dice score
         :return:
         """
 
         Logger.log("Creating some really cool metrics with the trained model <3", in_cli=True)
+
+        loss_function = CombinedLoss(job["training"]["loss"]["alpha"], job["training"]["loss"]["input_dim"])
 
         # Try resetting weights
         for layer in trained_model.children():
@@ -56,28 +57,73 @@ class ChenEvaluator(BaseEvaluator):
         # Define K-fold Cross Validator
         kfold = KFold(n_splits=self.k_folds, shuffle=True)
 
-        model = trained_model
+        # Define model and set to evaluation mode
+        model = trained_model.eval()
 
         # K-fold cross validation
-        for fold in enumerate(kfold.split(evaluation_set)):
+        for current_fold, fold in enumerate(kfold.split(evaluation_set)):
+
+            eval_running_loss = 0
+
+            # Initiate dice loss per organ and total
+            organ_dice_losses = {}
+            dice_loss_fn = DiceCoefficient()
 
             # Evaluation for current fold
-            correct, total = 0, 0
-            with torch.no_grad():
+            for batch, batch_input in enumerate(fold):
 
-                # Iterate over validation data and generate predictions
-                for i, data in enumerate(evaluation_set, 0):
+                with torch.no_grad():
 
                     # Get inputs and generate outputs
-                    inputs, targets = data
+                    inputs, labels = batch_input
                     outputs = model(inputs)
 
-                    # Set correct and total
-                    _, predicted = torch.max(outputs.data, 1)
-                    total += targets.size(0)
-                    correct += (predicted == targets).sum()#.item()
+                    # Calculate loss
+                    eval_loss = self.loss_function.forward(inputs, outputs)
 
-                print('Accuracy for fold %d: %d %%' % (fold, 100.0 * correct / total))
-                Logger.log("Accuracy for fold" + fold + ": " + 100.0 * correct / total)
+                    # Add to running validation loss
+                    eval_running_loss += eval_loss.detach().cpu().numpy()
 
-        return 100
+                    # Iterate through channels and compute dice losses for metric logging
+                    for i, organ in enumerate(job['training']['dataset']['labels']):
+                        sub_tensor = outputs[:, i, :, :, :]
+                        sub_label = labels[:, i, :, :, :]
+                        if organ not in organ_dice_losses.keys():
+                            organ_dice_losses[organ] = []
+                        organ_dice_losses[organ].append(float(dice_loss_fn(sub_tensor, sub_label)))
+                    if "Background" not in organ_dice_losses.keys():
+                        organ_dice_losses["Background"] = []
+                    organ_dice_losses["Background"].append(
+                        float(
+                            dice_loss_fn(
+                                outputs[:, len(job['training']['dataset']['labels']), :, :, :],
+                                labels[:, len(job['training']['dataset']['labels']), :, :, :],
+                            )
+                        )
+                    )
+
+                    # Get the current running los
+                    current_loss = eval_running_loss / batch if batch > 0 else eval_running_loss
+
+                    # Print epoch status bar
+                    Logger.print_status_bar(
+                        done=((batch + 1) / len(evaluation_set)) * 100,
+                        title="evaluation loss: " + "{:.5f}".format(current_loss)
+                    )
+
+                # Mean over the dice losses
+                for key, val in organ_dice_losses.items():
+                    organ_dice_losses[key] = sum(organ_dice_losses[key]) / len(organ_dice_losses[key])
+
+                # Print epoch status bar
+                Logger.print_status_bar(done=((batch + 1) / len(evaluation_set)) * 100, title="validating model")
+
+                # Print loss for last fold
+                Logger.log("Loss for fold" + current_fold + ": " + eval_loss)
+
+                # End status bar
+                Logger.end_status_bar()
+
+        # Return epoch train val loss
+        return eval_running_loss / len(evaluation_set)
+
