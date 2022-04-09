5202ff234791efff67b276b3086221e83f513775
Theo Rieken <mail@theorieken.de>
2022-04-09 17:52:22 +0200

    hot fix

--- src/Data/CTDataset.py
+++ src/Data/CTDataset.py
@@ -179,7 +179,7 @@ class CTDataset(Dataset):
         """
 
         # Return count of samples
-        return len(self.samples) * 8
+        return len(self.samples)
 
     def create_all_visualizations(self, direction: str = "vertical"):
         """
